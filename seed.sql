4e6a37e747b600ee5988d42a17db47b8badaed9c
solufemi91 <stevenoo@spartaglobal.com>
2017-11-18 20:46:21 +0000

    imported database

--- seed.sql
+++ seed.sql
@@ -1,14 +1,14 @@
-ROP TABLE IF EXISTS booksPost;
+DROP TABLE IF EXISTS bookspost;
 
-CREATE TABLE booksPost (
+CREATE TABLE bookspost (
 
   id SERIAL PRIMARY KEY,
   title VARCHAR(255),
-  body TEXT
+  body TEXT,
   image TEXT
 
 );
 
-insert into booksPost (title,body,image) values ("A Streetcar Named Desire","This is the first post","https://i.ebayimg.com/images/g/03IAAOSw4CFY0L30/s-l300.jpg");
-insert into booksPost (title,body,image) values ("To Kill a MockingBird", "This is the second post", "https://ritikab.files.wordpress.com/2010/07/tokillamockingbird3.jpg");
-insert into booksPost (title,body,image) values ("1000 Years of Solitude","This is the third post","https://images-na.ssl-images-amazon.com/images/I/41NpPQ3HNqL._SL300_.jpg");
+insert into bookspost (title,body,image) values ('A Streetcar Named Desire','This is the first post','https://i.ebayimg.com/images/g/03IAAOSw4CFY0L30/s-l300.jpg');
+insert into bookspost (title,body,image) values ('To Kill a MockingBird', 'This is the second post', 'https://ritikab.files.wordpress.com/2010/07/tokillamockingbird3.jpg');
+insert into bookspost (title,body,image) values ('1000 Years of Solitude','This is the third post','https://images-na.ssl-images-amazon.com/images/I/41NpPQ3HNqL._SL300_.jpg');
