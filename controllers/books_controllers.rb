34c5d9606e7153dd5209738a12add6b7a886eb36
solufemi91 <stevenoo@spartaglobal.com>
2017-11-19 15:05:25 +0000

    the home screen is working with the database

--- controllers/books_controllers.rb
+++ controllers/books_controllers.rb
@@ -7,31 +7,32 @@ class BooksController < Sinatra::Base
     register Sinatra::Reloader
  end
 
- $books = [{
- id: 0,
- title: "A Streetcar Named Desire",
- body: "This is the first post",
- image: "https://i.ebayimg.com/images/g/03IAAOSw4CFY0L30/s-l300.jpg"
-
-},
-{
-  id: 1,
-  title: "To Kill a MockingBird",
-  body: "This is the second post",
-  image: "https://ritikab.files.wordpress.com/2010/07/tokillamockingbird3.jpg"
-},
-{
-  id: 2,
-  title: "1000 Years of Solitude",
-  body: "This is the third post",
-  image: "https://images-na.ssl-images-amazon.com/images/I/41NpPQ3HNqL._SL300_.jpg"
-}]
+#  $books = [{
+#  id: 0,
+#  title: "A Streetcar Named Desire",
+#  body: "This is the first post",
+#  image: "https://i.ebayimg.com/images/g/03IAAOSw4CFY0L30/s-l300.jpg"
+#
+# },
+# {
+#   id: 1,
+#   title: "To Kill a MockingBird",
+#   body: "This is the second post",
+#   image: "https://ritikab.files.wordpress.com/2010/07/tokillamockingbird3.jpg"
+# },
+# {
+#   id: 2,
+#   title: "1000 Years of Solitude",
+#   body: "This is the third post",
+#   image: "https://images-na.ssl-images-amazon.com/images/I/41NpPQ3HNqL._SL300_.jpg"
+# }]
 
 
 
  get "/" do
    @title = "The books page"
-   @books = $books
+  #  @books = $books
+  @posts = Post.all
    erb :'books/index'
  end
 
