dc04a86259c41928a290c56cfa8ea7be2b474705
solufemi91 <stevenoo@spartaglobal.com>
2017-11-20 21:34:57 +0000

    erb shows

--- controllers/car_controller.rb
+++ controllers/car_controller.rb
@@ -1,9 +1,20 @@
 class CarController < Sinatra::Base
+  set :root, File.join(File.dirname(__FILE__), "..")
+  set :views, Proc.new { File.join(root, "views") }
+
+  configure :development do
+     register Sinatra::Reloader
+  end
+
 
   get "/cars" do
 
     # @title = "Car homepage"
     "This is a list of cars"
+    erb :'cars/index'
+
+
+
 
 
 
